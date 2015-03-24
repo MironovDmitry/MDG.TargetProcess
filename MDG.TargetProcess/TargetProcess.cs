0b3d2506d2a6ef8ca2151abdf33d82f29c0dfa84
Dmitry Mironov <dmitry.g.mironov@gmail.com>
2015-03-24 14:41:48 +0300

    moved to new folder

---
+++ MDG.TargetProcess/TargetProcess.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace MDG.TargetProcess
+{
+    public class TP
+    {
+        readonly static string _apiURL = Properties.Settings.Default.tpAPIURL;
+        readonly static string _token = Properties.Settings.Default.tpToken;
+
+        public List<User> Developers { get; set; }
+
+        public Uri BuildUri(URIOptions uriOptions)
+        {
+            StringBuilder uri = new StringBuilder(_apiURL);
+            if (!String.IsNullOrEmpty(uriOptions.EntityType))
+            {
+                uri.Append(uriOptions.EntityType);
+            }
+            else
+            {
+                uri.Append("userstories");
+            }
+            uri.Append("?");
+            if (!String.IsNullOrEmpty(uriOptions.IncludeStatement))
+            {
+                uri.Append("include=");
+                uri.Append(uriOptions.IncludeStatement);
+                uri.Append("&");
+            }
+            if (!String.IsNullOrEmpty(uriOptions.WhereStatement))
+            {
+                uri.Append("where=");
+                uri.Append(uriOptions.WhereStatement);
+                uri.Append("&");
+            }
+            uri.Append(_token);
+
+            return new Uri(uri.ToString());
+        }
+
+        public string GetResponseFromWebService(Uri uri)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Encoding = Encoding.UTF8;
+                webClient.Headers["Content-Type"] = "application/json; charset=UTF-8";
+
+                return webClient.DownloadString(uri);
+            }
+        }
+
+        public void GetDevelopers()
+        {
+            URIOptions uriOptions = new URIOptions();
+            uriOptions.EntityType = "users";
+            uriOptions.IncludeStatement = "[id,FirstName,LastName]";
+            uriOptions.WhereStatement = "(IsActive eq 'true') and (role.id eq 1)";
+
+            Uri uri = BuildUri(uriOptions);
+
+            string jsonResult = GetResponseFromWebService(uri);
+            dynamic result = JsonConvert.DeserializeObject(jsonResult);
+
+            List<User> developers = new List<User>();
+            foreach (var item in result.Items)
+            {
+                User developer = new User();
+                developer.Id = item.Id;
+                developer.LastName = item.LastName;
+                developer.FirstName = item.FirstName;
+
+                developers.Add(developer);
+                developer = null;
+            }
+
+            this.Developers = developers;
+        }
+
+        public void GetUserStoriesForUser(int userID)
+        {
+            URIOptions uriOptions = new URIOptions();
+            uriOptions.EntityType = "userstories";
+            uriOptions.WhereStatement = "(Owner.Id eq " + userID.ToString() + ")";
+        }        
+    }
+
+
+    public class URIOptions
+    {
+        public string EntityType { get; set; }
+        public string IncludeStatement { get; set; }
+        public string WhereStatement { get; set; }
+    }
+
+}
---
+++ MDG.TargetProcess/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace MDG.TargetProcess
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class User
+    {
+        public string Kind { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsAdministrator { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public double WeeklyAvailableHours { get; set; }
+        public int CurrentAllocation { get; set; }
+        public double CurrentAvailableHours { get; set; }
+        public DateTime? AvailableFrom { get; set; }
+        public int AvailableFutureAllocation { get; set; }
+        public double AvailableFutureHours { get; set; }
+        public bool IsObserver { get; set; }
+        public object Skills { get; set; }
+        public string ActiveDirectoryName { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public class Users
+    {
+        public string Next { get; set; }
+        public List<User> Items { get; set; }
+    }
+}
